a67019590536cbf72972c25c3245658bc25013ad
Hyunsense <hyunsense1022@gmail.com>
2023-04-13 14:12:29 +0900

    04/12 mini Project

---
+++ front/html3/javascript/index.js
@@ -0,0 +1,18 @@
+function func0() {
+    basic.style.display = "block";
+    american.style.display= "none";
+    japanese.style.display = "none";
+
+}
+// 일식
+function func1(){
+    basic.style.display = "none";
+    american.style.display= "block";
+    japanese.style.display = "none";
+}
+// 양식
+function func2(){
+    basic.style.display = "none";
+    american.style.display= "none";
+    japanese.style.display = "black";
+}
