876abaca83cd3c2084fb63d4f71678aa0472b853
HyunSense <hyunsense1022@gmail.com>
2023-04-14 02:23:41 +0900

    04/13 mini Project ing..

---
+++ front/letseat/javascript/index.js
@@ -0,0 +1,13 @@
+function func0() {
+    basic.style.display = "block";
+    wet.style.display= "none";
+
+}
+function func1(){
+    basic.style.display = "none";
+    wet.style.display= "block";
+}
+// function func2(){
+//     basic.style.display = "none";
+//     american.style.display= "block";
+// }
