b2560c39ffd1010aadcda60006c9ba4d8029b8bf
HyunSense <hyunsense1022@gmail.com>
2023-04-18 02:13:19 +0900

    hotplace fixed

---
+++ front/hotplace/js/index.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+
+    let selecs;
+    let selec
+    let selecCount = 0
+    let heartCount = 0;
+
+    selecs = document.querySelectorAll(".selec");
+    // selec = document.querySelector(".selec");
+    heartCount = document.getElementById("heartCount");
+
+    selecs.addEventListener("click", function(){
+        console.log(selecCount++);
+    });
+   
+    
+
+    
+
+
+   
+
+
+
+
+    
+
+
+
+
+
+
+};
