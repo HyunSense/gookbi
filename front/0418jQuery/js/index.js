d8a5130830cafa518b5364b87fdb71d752e303a2
Hyunsense <hyunsense1022@gmail.com>
2023-04-18 10:56:48 +0900

    jQuery Practice

--- front/0418jQuery/js/index.js
+++ front/0418jQuery/js/index.js
@@ -22,10 +22,49 @@ window.onload=function() {
     // }
     // document.write(sum);
 
-    let box = document.querySelector(".box");
-    let str="";
-    for(i=0; i<=9; i++){
-        str= str + "3 * " + i + " = " + 3*i;
-        box.innerHTML = str;
+    // let box = document.querySelector(".box");
+    // let str="";
+    // for(i=0; i<=9; i++){
+    //     str= str + "<br>   3 * " + i + " = " + 3*i;
+    //     box.innerHTML = str;
+    // }
+
+    // let num, ok, str="", box;
+    // ok=document.querySelector(".ok");
+    // num=document.querySelector(".num");
+    // box=document.querySelector(".box")
+    // ok.onclick=function(){
+    //     str="";
+    //     n = num.value;
+    //     for(i=1; i<=9; i++){
+    //         str+="<br>" + n + " * " + i + " = " + n*i;
+
+    //     }
+    //     box.innerHTML = str;
+    // }
+    // ok.addEventListener("click", function() {
+    //     str="";
+    //     n = num.value;
+    //     for(i=1; i<=9; i++){
+    //         str+="<br>" + n + " * " + i + " = " + n*i;
+
+    //     }
+    //     box.innerHTML = str;
+    // })
+
+    ok=document.querySelector(".ok");
+    ok.addEventListener("click",func);
+}
+function func() {
+    let num, ok, str="", box;
+    num=document.querySelector(".num");
+    box=document.querySelector(".box");
+
+    str="";
+    n = num.value;
+    for(i=1; i<=9; i++){
+        str+="<br>" + n + " * " + i + " = " + n*i;
+
     }
+    box.innerHTML = str;
 }
