9825b01e981a9da1852b405c9ce462592cea98bf
Hyunsense <hyunsense1022@gmail.com>
2023-04-24 17:34:24 +0900

    blog2.html, index3.js, index3.css modified

--- front/code-blog/js/index3.js
+++ front/code-blog/js/index3.js
@@ -16,7 +16,7 @@ window.onload = function(){
     $(bgBlack).each(function(i){
         $(bgBlack[i]).hover(function(){
         console.log("hovering--");
-        $(imgTitle[i]).stop().animate({left:"99px",top:"100px"},300);
+        $(imgTitle[i]).stop().animate({left:"87px",top:"100px"},300);
     },function(){
         $(imgTitle[i]).stop().animate({left:"12px",top:"238px"},300);
     });
@@ -29,7 +29,8 @@ window.onload = function(){
     $(bgBlack).each(function(i){
         $(bgBlack[i]).on("click",function(){
         // $(".layout5").animate({top:"50px"},400).show();    
-        $(".layout5").animate({top:"50px"},400).show();    
+        $(".layout5").animate({top:"50px"},400).show(); 
+        $(".overRay").show();   
         });
     });
 
@@ -37,6 +38,13 @@ window.onload = function(){
 
         $(".layout5").animate({top:"1000px"},200,function(){
         $(".layout5").hide()});
+        $(".overRay").hide();
+    });
+
+    $(".overRay").on("click", function(){
+        console.log("hide");
+        $(".layout5").hide();
+        $(this).hide();
     });
     
 
